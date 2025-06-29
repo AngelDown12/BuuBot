0d7a97b4a08a27c9ecd8b0a9510d404a983ca428
AngelDown12 <angeldown12xz@gmail.com>
2025-06-28 23:41:11 -0600

    Actualizar group-unbanchat.js

--- plugins/group-unbanchat.js
+++ plugins/group-unbanchat.js
@@ -1,11 +1,30 @@
-let handler = async (m, { conn, isAdmin, isROwner} ) => {
-    if (!(isAdmin || isROwner)) return dfail('admin', m, conn)
-    global.db.data.chats[m.chat].isBanned = false
-    await conn.reply(m.chat, '✅ Bot activo en este grupo.', m, )
-    await m.react('✅')
+let handler = async (m, { conn, isAdmin, isROwner }) => {
+  if (!(isAdmin || isROwner)) {
+    return dfail('admin', m, conn)
+  }
+
+  global.db.data.chats[m.chat].isBanned = false
+
+  await conn.sendMessage(m.chat, {
+    text: '✅ 𝐀𝐧𝐠𝐞𝐥 𝐁𝐨𝐭 𝐀𝐜𝐭𝐢𝐯𝐨.',
+    contextInfo: {
+      externalAdReply: {
+        title: '𝐀𝐧𝐠𝐞𝐥 𝐁𝐨𝐭 𝐃𝐞𝐥𝐚𝐲',
+        body: '𝐀𝐧𝐠𝐞𝐥 𝐁𝐨𝐭 𝐃𝐞𝐥𝐚𝐲',
+        mediaType: 1,
+        thumbnailUrl: 'https://qu.ax/JRCMQ.jpg',
+        renderLargerThumbnail: false,
+        sourceUrl: ''
+      }
+    }
+  }, { quoted: m })
+
+  await m.react('✅')
 }
+
 handler.help = ['desbanearbot']
 handler.tags = ['group']
 handler.command = ['desbanearbot', 'unbanchat']
-handler.group = true 
+handler.group = true
+
 export default handler
