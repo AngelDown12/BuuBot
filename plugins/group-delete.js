af6691a247755dd5139ae8b9bfa29c5a106de14f
AngelDown12 <angeldown12xz@gmail.com>
2025-06-28 23:18:39 -0600

    Actualizar group-delete.js

--- plugins/group-delete.js
+++ plugins/group-delete.js
@@ -1,14 +1,38 @@
 let handler = async (m, { conn, usedPrefix, command }) => {
+  if (!m.quoted) {
+    return conn.sendMessage(m.chat, {
+      text: `🚩 Responde al mensaje que deseas eliminar.`,
+      contextInfo: {
+        externalAdReply: {
+          title: '𝐀𝐧𝐠𝐞𝐥 𝐁𝐨𝐭 𝐃𝐞𝐥𝐚𝐲',
+          body: '𝐀𝐧𝐠𝐞𝐥 𝐁𝐨𝐭 𝐃𝐞𝐥𝐚𝐲',
+          mediaType: 1,
+          thumbnailUrl: 'https://qu.ax/JRCMQ.jpg',
+          renderLargerThumbnail: false,
+          sourceUrl: ''
+        }
+      }
+    }, { quoted: m })
+  }
 
-if (!m.quoted) return conn.reply(m.chat, `🚩 Responde al mensaje que deseas eliminar.`, m, )
-try {
-let delet = m.message.extendedTextMessage.contextInfo.participant
-let bang = m.message.extendedTextMessage.contextInfo.stanzaId
-return conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }})
- } catch {
-return conn.sendMessage(m.chat, { delete: m.quoted.vM.key })
+  try {
+    let delet = m.message.extendedTextMessage.contextInfo.participant
+    let bang = m.message.extendedTextMessage.contextInfo.stanzaId
+    return conn.sendMessage(m.chat, {
+      delete: {
+        remoteJid: m.chat,
+        fromMe: false,
+        id: bang,
+        participant: delet
+      }
+    })
+  } catch {
+    return conn.sendMessage(m.chat, {
+      delete: m.quoted.vM.key
+    })
+  }
 }
-}
+
 handler.help = ['delete']
 handler.tags = ['group']
 handler.command = /^del(ete)?$/i
