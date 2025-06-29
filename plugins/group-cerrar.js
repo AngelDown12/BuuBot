b128c3d81dca4373c137e263475c8de28e8f6e83
AngelDown12 <angeldown12xz@gmail.com>
2025-06-29 02:10:49 -0600

    Actualizar group-cerrar.js

--- plugins/group-cerrar.js
+++ plugins/group-cerrar.js
@@ -1,35 +1,41 @@
 const handler = (m, { conn }) => {
-  if (!m.isGroup || !m.isGroupAdmin) return
+  if (!m.isGroup) return
+  conn.groupMetadata(m.chat).then(meta => {
+    const admins = meta.participants.filter(p => p.admin).map(p => p.id)
+    const isAdmin = admins.includes(m.sender)
 
-  conn.groupSettingUpdate(m.chat, 'announcement')
-    .then(() => {
-      conn.sendMessage(m.chat, {
-        text: '🔒 *Grupo cerrado solo para admins.*',
-        contextInfo: {
-          externalAdReply: {
-            title: "𝐀𝐧𝐠𝐞𝐥 𝐁𝐨𝐭 𝐃𝐞𝐥𝐚𝐲",
-            body: "𝐀𝐧𝐠𝐞𝐥 𝐁𝐨𝐭 𝐃𝐞𝐥𝐚𝐲",
-            thumbnailUrl: "https://qu.ax/JRCMQ.jpg",
-            renderLargerThumbnail: false,
-            sourceUrl: ''
+    if (!isAdmin) return
+
+    conn.groupSettingUpdate(m.chat, 'announcement')
+      .then(() => {
+        conn.sendMessage(m.chat, {
+          text: '🔒 *Grupo cerrado solo para admins.*',
+          contextInfo: {
+            externalAdReply: {
+              title: "𝐀𝐧𝐠𝐞𝐥 𝐁𝐨𝐭 𝐃𝐞𝐥𝐚𝐲",
+              body: "𝐀𝐧𝐠𝐞𝐥 𝐁𝐨𝐭 𝐃𝐞𝐥𝐚𝐲",
+              thumbnailUrl: "https://qu.ax/JRCMQ.jpg",
+              renderLargerThumbnail: false,
+              sourceUrl: ''
+            }
           }
-        }
-      }, { quoted: m })
-    })
-    .catch(() => {
-      conn.sendMessage(m.chat, {
-        text: '❌ No tengo permisos para cerrar el grupo.',
-        contextInfo: {
-          externalAdReply: {
-            title: "𝐀𝐧𝐠𝐞𝐥 𝐁𝐨𝐭 𝐃𝐞𝐥𝐚𝐲",
-            body: "𝐀𝐧𝐠𝐞𝐥 𝐁𝐨𝐭 𝐃𝐞𝐥𝐚𝐲",
-            thumbnailUrl: "https://qu.ax/JRCMQ.jpg",
-            renderLargerThumbnail: false,
-            sourceUrl: ''
+        }, { quoted: m })
+      })
+      .catch(() => {
+        conn.sendMessage(m.chat, {
+          text: '❌ No tengo permisos para cerrar el grupo.',
+          contextInfo: {
+            externalAdReply: {
+              title: "𝐀𝐧𝐠𝐞𝐥 𝐁𝐨𝐭 𝐃𝐞𝐥𝐚𝐲",
+              body: "𝐀𝐧𝐠𝐞𝐥 𝐁𝐨𝐭 𝐃𝐞𝐥𝐚𝐲",
+              thumbnailUrl: "https://qu.ax/JRCMQ.jpg",
+              renderLargerThumbnail: false,
+              sourceUrl: ''
+            }
           }
-        }
-      }, { quoted: m })
-    })
+        }, { quoted: m })
+      })
+  })
 }
 
 handler.customPrefix = /^cerrar$/i
