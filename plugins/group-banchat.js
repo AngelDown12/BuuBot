7bfb6272fdf6a0f223648663d5c94a41a3c90bd1
AngelDown12 <angeldown12xz@gmail.com>
2025-06-28 23:10:20 -0600

    Actualizar group-banchat.js

--- plugins/group-banchat.js
+++ plugins/group-banchat.js
@@ -1,11 +1,26 @@
 let handler = async (m, { conn, isAdmin, isROwner }) => {
-    if (!(isAdmin || isROwner)) return dfail('admin', m, conn)
-    global.db.data.chats[m.chat].isBanned = true
-    await conn.reply(m.chat, `🚫 BARBOZABOT FUE DESACTIVADO EN ESTE CHAT`, m, )
-    await m.react('☑️')
+  if (!(isAdmin || isROwner)) return dfail('admin', m, conn)
+  global.db.data.chats[m.chat].isBanned = true
+
+  await conn.sendMessage(m.chat, {
+    text: '🚫 𝐀𝐧𝐠𝐞𝐥 𝐁𝐨𝐭 fue desactivado en este chat',
+    contextInfo: {
+      externalAdReply: {
+        title: '𝐀𝐧𝐠𝐞𝐥 𝐁𝐨𝐭 𝐃𝐞𝐥𝐚𝐲 ',
+        body: '𝐀𝐧𝐠𝐞𝐥 𝐁𝐨𝐭 𝐃𝐞𝐥𝐚𝐲 ',
+        mediaType: 1,
+        thumbnailUrl: 'https://qu.ax/JRCMQ.jpg',
+        renderLargerThumbnail: false,
+        sourceUrl: ''
+      }
+    }
+  }, { quoted: m })
+
+  await m.react('☑️')
 }
 handler.help = ['banearbot']
 handler.tags = ['group']
 handler.command = ['banearbot', 'banchat']
 handler.group = true 
+
 export default handler
