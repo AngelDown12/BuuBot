8cfbc12b413d86c721dcfa461a8023ce93a92c9d
AngelDown12 <angeldown12xz@gmail.com>
2025-06-28 23:26:50 -0600

    Actualizar group-gcdescripción.js

--- plugins/group-gcdescripción.js
+++ plugins/group-gcdescripción.js
@@ -1,11 +1,26 @@
-const handler = async (m, {conn, args}) => {
-  await conn.groupUpdateDescription(m.chat, `${args.join(' ')}`);
-  m.reply('*👑 La descripción del grupo se modifico correctamente*');
-};
-handler.help = ['groupdesc <text>'];
-handler.tags = ['grupo'];
+const handler = async (m, { conn, args }) => {
+  await conn.groupUpdateDescription(m.chat, `${args.join(' ')}`)
+
+  await conn.sendMessage(m.chat, {
+    text: '👑 ',
+    contextInfo: {
+      externalAdReply: {
+        title: '𝐀𝐧𝐠𝐞𝐥 𝐁𝐨𝐭 𝐃𝐞𝐥𝐚𝐲',
+        body: '𝐀𝐧𝐠𝐞𝐥 𝐁𝐨𝐭 𝐃𝐞𝐥𝐚𝐲',
+        mediaType: 1,
+        thumbnailUrl: 'https://qu.ax/JRCMQ.jpg',
+        renderLargerThumbnail: false,
+        sourceUrl: ''
+      }
+    }
+  }, { quoted: m })
+}
+
+handler.help = ['groupdesc <text>']
+handler.tags = ['grupo']
 handler.command = ['gpdesc', 'groupdesc']
-handler.group = true;
-handler.admin = true;
-handler.botAdmin = true;
-export default handler;
+handler.group = true
+handler.admin = true
+handler.botAdmin = true
+
+export default handler
