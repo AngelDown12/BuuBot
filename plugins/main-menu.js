9c7f2f6db58b5721145fd45335a95dbbdb0d6af0
AngelDown12 <angeldown12xz@gmail.com>
2025-07-01 21:11:46 -0600

    Actualizar main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -1,26 +1,19 @@
-let handler = async (m, { conn }) => {
-  const texto = `✨ *MENÚ PRINCIPAL* ✨
+await conn.sendMessage(
+  m.chat,
+  {
+    text: `✨ *MENÚ PRINCIPAL* ✨
 
 Hola 👋 ${m.sender.split('@')[0]}
-
-Elige una opción 👇`;
-
-  const buttons = [
-    { buttonId: '.info', buttonText: { displayText: '📚 Info' }, type: 1 },
-    { buttonId: '.donar', buttonText: { displayText: '💸 Donar' }, type: 1 },
-    { buttonId: '.owner', buttonText: { displayText: '👑 Owner' }, type: 1 }
-  ];
-
-  const buttonMessage = {
-    text: texto,
+Elige una opción 👇`,
     footer: 'Bot Angel 👑',
-    buttons: buttons,
-    headerType: 1
-  };
-
-  await conn.sendMessage(m.chat, buttonMessage, { quoted: m });
-};
-
-handler.command = /^menu$/i;
-
-export default handler;
+    templateButtons: [
+      {
+        index: 1,
+        quickReplyButton: {
+          displayText: '👑 Owner',
+          id: '.owner'
+        }
+      }
+    ]
+  }
+)
