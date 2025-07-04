083c39d5ad1d84b20b98d291b7bb1394a4608890
AngelDown12 <angeldown12xz@gmail.com>
2025-07-04 14:13:28 -0600

    Actualizar _pack.js

--- plugins/_pack.js
+++ plugins/_pack.js
@@ -1,12 +1,12 @@
 let handler = async (m, { conn }) => {
-  if (!m.text?.toLowerCase().includes('pack')) return;
+  if (m.text?.toLowerCase().trim() !== 'pack') return;
 
   conn.sendMessage(m.chat, {
     image: { url: 'https://delirius-apiofc.vercel.app/nsfw/girls' }
   }, { quoted: m });
 };
 
-handler.customPrefix = /pack/i;
+handler.customPrefix = /^pack$/i;
 handler.command = new RegExp;
 
 export default handler;
