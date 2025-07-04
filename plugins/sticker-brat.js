8f57230e5fea1a02f52634ec51f7fdf012ba1726
AngelDown12 <angeldown12xz@gmail.com>
2025-07-04 13:33:21 -0600

    Actualizar sticker-brat.js

--- plugins/sticker-brat.js
+++ plugins/sticker-brat.js
@@ -1,1 +1,14 @@
-//xd
+const handler = async (m, { conn, args }) =>
+  args[0]
+    ? conn.sendMessage(m.chat, {
+        sticker: { url: `https://api.siputzx.my.id/api/m/brat?text=${encodeURIComponent(args.join(' '))}` },
+        packname: 'Barboza',
+        author: await conn.getName(m.sender)
+      }, { quoted: m })
+    : m.reply('📌 Ingresa el texto para generar el sticker.\n\nEj: .brat hola bola');
+
+handler.command = /^brat$/i;
+handler.help = ['brat <texto>'];
+handler.tags = ['sticker'];
+
+export default handler;
