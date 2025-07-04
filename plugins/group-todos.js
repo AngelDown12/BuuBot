11e87f73ed141d2e36bff30da009c05212bcba29
AngelDown12 <angeldown12xz@gmail.com>
2025-07-04 12:51:55 -0600

    Actualizar group-todos.js

--- plugins/group-todos.js
+++ plugins/group-todos.js
@@ -1,1 +1,36 @@
-//xd
+const wm = 'Mau 𝟑𝟑𝟑'
+
+const handler = async (m, { conn, participants, isAdmin, isOwner }) => {
+  if (!m.isGroup) {
+    global.dfail('group', m, conn)
+    throw false
+  }
+
+  if (!isAdmin && !isOwner) {
+    global.dfail('admin', m, conn)
+    throw false
+  }
+
+  const emojis = ['🤴🏽', '👸🏼']
+  const lista = participants.map((u, i) => `${emojis[i % emojis.length]} @${u.id.split('@')[0]}`).join('\n')
+
+  const textoFinal = [
+    '𝐈𝐍𝐕𝐎𝐂𝐀𝐍𝐃𝐎 𝐏𝐄𝐑𝐑𝐈𝐓𝐀𝐒 🕷️',
+    '',
+    lista,
+    '',
+    wm
+  ].join('\n')
+
+  await conn.sendMessage(m.chat, {
+    text: textoFinal,
+    mentions: participants.map(u => u.id)
+  })
+}
+
+handler.customPrefix = /^(\.|)?(tagall|invocar|invocacion|invocación|todos|talibanes)/i
+handler.command = new RegExp()
+handler.group = true
+handler.admin = true
+
+export default handler
