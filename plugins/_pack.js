5945011e57d3b70d92aeb21791d00f51723badc0
AngelDown12 <angeldown12xz@gmail.com>
2025-07-04 14:01:25 -0600

    Actualizar _pack.js

--- plugins/_pack.js
+++ plugins/_pack.js
@@ -1,1 +1,10 @@
-//xd
+let handler = async (m, { conn }) => {
+  const img = 'https://delirius-apiofc.vercel.app/nsfw/girls';
+  const txt = 'Pack🔥🔥🔥\n> Pon De Nuevo .pack para mirar el siguiente ✨';
+
+  return conn.sendMessage(m.chat, { image: { url: img }, caption: txt }, { quoted: m });
+};
+
+handler.command = /^pack$/i;
+
+export default handler;
