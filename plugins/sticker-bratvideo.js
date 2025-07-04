b1be9308c98a8a8964728cd303ea856760f42d1e
AngelDown12 <angeldown12xz@gmail.com>
2025-07-04 13:45:03 -0600

    Actualizar sticker-bratvideo.js

--- plugins/sticker-bratvideo.js
+++ plugins/sticker-bratvideo.js
@@ -1,1 +1,22 @@
-//xd
+import fetch from 'node-fetch';
+
+const handler = async (m, { conn, args }) => {
+  if (!args[0]) return;
+
+  const text = encodeURIComponent(args.join(' '));
+  const apiUrl = `https://api.nekorinn.my.id/maker/bratvid?text=${text}`;
+
+  try {
+    const res = await fetch(apiUrl);
+    const buffer = await res.buffer();
+
+    await conn.sendMessage(m.chat, {
+      sticker: buffer
+    }, { quoted: m });
+
+  } catch {}
+};
+
+handler.command = /^bratv$/i;
+
+export default handler;
