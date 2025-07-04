1b68790dd83bdd97906f76d2ec0d4dd683571322
AngelDown12 <angeldown12xz@gmail.com>
2025-07-04 14:43:16 -0600

    Actualizar downloader-soundcloud.js

--- plugins/downloader-soundcloud.js
+++ plugins/downloader-soundcloud.js
@@ -176,7 +176,7 @@ const handler = async (m, { conn, text, usedPrefix, command }) => {
 
 handler.command = handler.help = ["play", "play2", "ytmp3", "yta", "ytmp4", "ytv"];
 handler.tags = ["downloader"];
-handler.register = true
+handler.register = false
 
 export default handler;
 
